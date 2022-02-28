3cc28ff016a4492069b2589ced79a967df3ca15c
Giribabu Velamunaidu <giribabuvelamunaidu@AGLS-MLT-483.local>
2022-02-28 10:55:23 +0530

    task3

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Github from './Components/github';
+import Index from "./Components/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Index />
+      {/* <Github />  */}
+    </div>
+  );
+}
+
+export default App;
